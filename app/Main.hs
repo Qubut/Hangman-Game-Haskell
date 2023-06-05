8c2b93d6ab6c770ec94d28de216d31af3b448052
Qubut <s-aahmed@haw-landshut.de>
2023-06-05 21:29:00 +0200

    Intialized the haskall hangman game project

---
+++ app/Main.hs
@@ -0,0 +1,10 @@
+module Main (main) where
+
+import Lib
+import Control.Monad (forever)
+import Data.Char (toLower)
+import Data.Maybe (isJust)
+import Data.List (intersperse)
+import System.Exit (exitSuccess)
+import System.Random (randomRIO) 
+
